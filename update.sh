fcfc51fece281b11cf654d9dff8c60c37d6b91aa
Tyler Hollingsworth <tyholling@gmail.com>
2026-03-22 15:42:02 -0700

    update update script

--- update.sh
+++ update.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 files=(
-  .ackrc
-  .config/nvim/init.vim
-  .gitconfig
-  .inputrc
-  .profile
-  .ssh/config
-  .tmux.conf
-  .vimrc
+	.ackrc
+	.gitconfig
+	.inputrc
+	.profile
+	.ssh/config
+	.tmux.conf
+	.vimrc
 )
 
 for i in ${files[@]}; do
-  [[ -e ~/$i ]] && cp ~/$i ./$i
+	[[ -e ~/$i ]] && cp ~/$i ./$i
 done
